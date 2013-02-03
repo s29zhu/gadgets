8ca8ac84dfb401b35c10d42c4c44828cfa1f8458
s29zhu <shashazhu1989@gmail.com>
2013-02-03 14:27:33 -0500

    size of c++ usually types from lab desktop

---
+++ size_of_types/get_size.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    cout<<"char "<<sizeof(char)<<endl;
+    cout<<"int  "<<sizeof(int)<<endl;
+    cout<<"long int "<<sizeof(long)<<endl;
+    cout<<"long long    "<<sizeof(long long)<<endl;
+    cout<<"float    "<<sizeof(float)<<endl;
+    cout<<"double   "<<sizeof(double)<<endl;
+    cout<<"long double  "<<sizeof(long double)<<endl;
+    cout<<"wchar_t  "<<sizeof(wchar_t)<<endl;
+    return 1;
+}
