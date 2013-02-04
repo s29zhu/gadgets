ce303773d528fc493c84f932c55447a7a9c1197c
s29zhu <shashazhu1989@gmail.com>
2013-02-04 11:27:03 -0500

    update get_size.cpp

---
+++ containers/vectors/vector.cpp
@@ -0,0 +1,17 @@
+/*
+*Author:s29zhu
+*Feb 2013
+*this is a file that test container vector member functions
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void){
+    std::vector<int> vector_test_1(20), vector_test_1;
+    
+    return 1;
+}
+
--- size_of_types/get_size.cpp
+++ size_of_types/get_size.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 int main(void){
     cout<<"char "<<sizeof(char)<<endl;
+    cout<<"unsigned int "<<sizeof(unsigned int)<<endl;
     cout<<"int  "<<sizeof(int)<<endl;
     cout<<"long int "<<sizeof(long)<<endl;
+    cout<<"unsigned long "<<sizeof(unsigned long)<<endl;
     cout<<"long long    "<<sizeof(long long)<<endl;
     cout<<"float    "<<sizeof(float)<<endl;
     cout<<"double   "<<sizeof(double)<<endl;
